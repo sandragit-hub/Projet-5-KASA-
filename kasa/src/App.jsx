f9eed141bdf38a7168d8a39b0c3baeb5fe463847
Sandra <sandra.rossi17@hotmail.com>
2024-11-05 19:47:06 +0100

    correction Router plus de message d'erreur

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './router';
 import Header from './Header/Header'
+
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <AppRouter />
 
-    </>
+    </Router>
   );
 }
 
