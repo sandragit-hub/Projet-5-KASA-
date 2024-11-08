aa35559f3a9309f63225ca5b4f7b14a773a9624d
Sandra <sandra.rossi17@hotmail.com>
2024-11-08 11:36:53 +0100

    ajout du composant collapse, + css + mise en page de la page à propos

--- kasa/src/assets/pages/Home.jsx
+++ kasa/src/assets/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Banner } from "../../components/banner/Banner";
 import { Card } from "../../components/card/card";
-
+import banner from "../../assets/image/banner.png"
 
 export default function Home() {
     return (
         <div>
-            <Banner />
-            <div className="divCard">
-                <Card />
+            <Banner image={banner} text="Chez vous, partout et ailleurs" showShadow={true} />
+            <div key={Card.id} className="divCard">
+                <Card>
+
+                </Card>
                 <Card />
                 <Card />
                 <Card />
