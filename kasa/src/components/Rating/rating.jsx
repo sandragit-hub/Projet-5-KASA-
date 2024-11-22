297e9ebf88d76d69bd4375537bfea4d2f9e964ba
Sandra <sandra.rossi17@hotmail.com>
2024-11-22 15:42:15 +0100

    scss page location

--- kasa/src/components/Rating/rating.jsx
+++ kasa/src/components/Rating/rating.jsx
@@ -9,7 +9,7 @@ export default function Rating({ rating }) {
             {[...Array(5)].map((star, index) => {
                 const ratingValue = index + 1;
                 return (
-                    <img key={index} src={ratingValue <= rating ? redStar : greyStar} alt="star" />
+                    <img key={index} src={ratingValue <= rating ? redStar : greyStar} alt="star" className='star' />
                 )
             })}
         </div>
